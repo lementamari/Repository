2c334d485352d7e3759a3f1d8a0a7007246fee17
Diana <diana10august@gmail.com>
2026-02-23 01:28:31 +0200

    Added some code

--- utils.py
+++ utils.py
@@ -34,3 +34,15 @@ def is_prime(n):
         return False
 
     return True
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def square(n):
+    return n * n
+
+def factorial(n):
+    result = 1
+    for i in range(2, n+1):
+        result *= i
+    return result
